9aaa19d35404b63cdaca2afbb8433d675bad34bc
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-02-05 16:45:53 -0600

    fixed flow of prompt questions

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const inquirer = require("inquirer")
-const { writeFile } = require('fs').promises;
+const fs = require('fs')
 const generateHTML = require("./src/template.js")
 
+const Engineer = require("./lib/engineer")
+const Intern = require("./lib/intern")
+const Manager = require("./lib/manager")
+const Employee = require("./lib/employee")
 
-const promptUser = () => {
-  return inquirer.prompt([
+let newEmployee =[];
+
+const promptUser = async () => {
+  const data = await inquirer
+  
+  .prompt([
+   
     {
       type: 'input',
       name: 'emName',
-      message: 'Employee Full Name: ',
-    },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'Employee Job Title:',
+      message: 'Full Name: ',
     },
     {
       type: 'input',
@@ -23,36 +27,111 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'email',
-      message: 'Employee Email Address:',
+      message: 'Email Address:',
     },
-    
-    //need to add logic around role selection 
+    {
+      type: 'list',
+      name: 'title',
+      message: 'Job Title:',
+      choices: ['Manager','Engineer','Intern', 'Other'],
+    },
+
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'Office Number:',
+      when: (newEmployee) => newEmployee.title === 'Manager',
+    },
 
     {
       type: 'input',
       name: 'github',
-      message: 'Engineer GitHub Username:',
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'Manager office number:',
+      message: 'GitHub Username:',
+      when: (newEmployee) => newEmployee.title === 'Engineer',
     },
 
     {
       type: 'input',
       name: 'school',
-      message: 'Intern school:',
+      message: 'School:',
+      when: (newEmployee) => newEmployee.title === 'Intern',
     },
 
-  ]);
+    {
+      type: 'input',
+      name: 'other',
+      message: 'Job Title:',
+      when: (newEmployee) => newEmployee.title === 'Other',
+    },
+
+    {
+      type: 'list',
+      name: 'next',
+      message: 'Select one',
+      choices: ['Add More', 'Complete Creation']
+    }
+
+  ])
+
+  if (data.title === "Manager") {
+    const newManager = new Manager (
+      data.emName,
+      data.eid,
+      data.email,
+      data.title,
+      data.officeNumber,
+    )
+    newEmployee.push(newManager)
+    console.log(newManager);
+  } else if (data.title === "Engineer") {
+    const newEngineer = new Engineer (
+      data.emName,
+      data.eid,
+      data.email,
+      data.title,
+      data.github,
+    )
+    newEmployee.push(newEngineer)
+    console.log(newEngineer);
+  } else if (data.title === "Intern"){
+    const newIntern = new Intern (
+      data.emName,
+      data.eid,
+      data.email,
+      data.title,
+      data.school,
+    )
+    newEmployee.push(newIntern)
+    console.log(newIntern);
+  } else {
+
+    //might remove the 'other' option
+    const newOther = new Employee (
+      data.emName,
+      data.eid,
+      data.email,
+      data.other,
+    )
+    newEmployee.push(newOther)
+    console.log(newEmployee)
+  }
+
+
+  if (data.next === "Add More"){
+    return promptUser()
+  } else {
+    return createProfile()
+  }
 };
 
-const init = () => {
-  promptUser()
-    .then((newEmployee) => writeFile('./dist/index.html', generateHTML(newEmployee)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
 
-init();
+promptUser()
+
+
+const createProfile = () => {
+  fs.writeFileSync('./dist/index.html', generateHTML(newEmployee));
+}
+
+
+
+
