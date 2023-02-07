10d399d3a3e61efc94436b13cb5bd0855fb2f8ef
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-02-06 20:36:58 -0600

    more template work

--- src/template.js
+++ src/template.js
@@ -1,17 +1,6 @@
 const generateHTML = (newEmployee) => {
 
-
-    let firstName = newEmployee[0].emName;
-    let firstID = newEmployee[0].id;
-    let firstEmail = newEmployee[0].email;
-
-    let secondName = newEmployee[1].emName;
-    let secondID = newEmployee[1].id;
-    let secondEmail = newEmployee[1].email;
-
-  
-    return `
-    <!DOCTYPE html>
+    const startHTML = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -23,10 +12,99 @@ const generateHTML = (newEmployee) => {
     <body>
 
     <header>
-
     <h1>Employee Profiles</h1>
+    </header>`
 
-    </header>
+
+if (newEmployee.length == 3 ) {
+    let firstName = newEmployee[0].emName;
+    let firstID = newEmployee[0].id;
+    let firstEmail = newEmployee[0].email;
+
+    
+    let firstAdditional 
+    let firstTitle 
+
+if (newEmployee[0].github) {
+    firstAdditional = newEmployee[0].github;
+    firstTitle = 'Engineer';
+} else if (newEmployee[0].school) {
+    firstAdditional = newEmployee[0].school
+    firstTitle = 'Intern';
+} else {
+    firstAdditional = newEmployee[0].officeNumber;
+    firstTitle = 'Manager';
+}
+
+    let firstHTML = ` <ul>
+    <li>${firstName}</li>
+    <li>${firstTitle}</li>
+    <li>${firstID}</li>
+    <li>${firstEmail}</li>
+    <li>${firstAdditional}</li>
+    </ul> `
+
+
+    let secondName = newEmployee[1].emName;
+    let secondID = newEmployee[1].id;
+    let secondEmail = newEmployee[1].email;
+
+    let secondAdditional 
+    let secondTitle 
+
+if (newEmployee[1].github) {
+    secondAdditional = newEmployee[1].github;
+    secondTitle = 'Engineer';
+} else if (newEmployee[1].school) {
+    secondAdditional = newEmployee[1].school
+    secondTitle = 'Intern';
+} else {
+    firstAdditional = newEmployee[1].officeNumber;
+    secondTitle = 'Manager';
+}
+    
+    let secondHTML = ` <ul>
+
+    <li>${secondName}</li>
+    <li>${secondTitle}</li>
+    <li>${secondID}</li>
+    <li>${secondEmail}</li>
+    <li>${secondAdditional}</li>
+    
+    </ul> `
+
+    let thirdName = newEmployee[2].emName;
+    let thirdID = newEmployee[2].id;
+    let thirdEmail = newEmployee[2].email;
+
+
+    let thirdAdditional 
+    let thirdTitle 
+
+if (newEmployee[2].github) {
+    thirdAdditional = newEmployee[2].github;
+    thirdTitle = 'Engineer';
+} else if (newEmployee[2].school) {
+    thirdAdditional = newEmployee[2].school
+    thirdTitle = 'Intern';
+} else {
+    thirdAdditional = newEmployee[2].officeNumber;
+    thirdTitle = 'Manager';
+}
+    
+    let thirdHTML = ` <ul>
+
+    <li>${thirdName}</li>
+    <li>${thirdTitle}</li>
+    <li>${thirdID}</li>
+    <li>${thirdEmail}</li>
+    <li>${thirdAdditional}</li>
+    
+    </ul> `
+
+
+    return `
+    ${startHTML}
 
     <section class="container">
 
@@ -34,11 +112,8 @@ const generateHTML = (newEmployee) => {
 
     <h1>Employee One</h1>
 
-    <ul>
-    <li>${firstName}</li>
-    <li>${firstEmail}</li>
-    <li>${firstID}</li>
-    </ul>
+    ${firstHTML}
+   
 
     </div>
 
@@ -46,18 +121,161 @@ const generateHTML = (newEmployee) => {
 
     <h1>Employee Two</h1>
 
-    <ul>
-    <li>${secondName}</li>
-    <li>${secondEmail}</li>
-    <li>${secondID}</li>
-    </ul>
+    ${secondHTML}
+    
+    </div>
 
     </div>
+
+    <div class="card">
+
+    <h1>Employee Three</h1>
+
+    ${thirdHTML}
+    
+    </div>
+
     </section>
 
     </body>
     </html>
     `
+} 
+
+
+    else if  (newEmployee.length == 2 ) {
+        let firstName = newEmployee[0].emName;
+        let firstID = newEmployee[0].id;
+        let firstEmail = newEmployee[0].email;
+
+        
+        let firstAdditional 
+        let firstTitle 
+
+    if (newEmployee[0].github) {
+        firstAdditional = newEmployee[0].github;
+        firstTitle = 'Engineer';
+    } else if (newEmployee[0].school) {
+        firstAdditional = newEmployee[0].school
+        firstTitle = 'Intern';
+    } else {
+        firstAdditional = newEmployee[0].officeNumber;
+        firstTitle = 'Manager';
+    }
+
+        let firstHTML = ` <ul>
+        <li>${firstName}</li>
+        <li>${firstTitle}</li>
+        <li>${firstID}</li>
+        <li>${firstEmail}</li>
+        <li>${firstAdditional}</li>
+        </ul> `
+
+
+        let secondName = newEmployee[1].emName;
+        let secondID = newEmployee[1].id;
+        let secondEmail = newEmployee[1].email;
+
+        let secondAdditional 
+        let secondTitle 
+
+    if (newEmployee[1].github) {
+        secondAdditional = newEmployee[1].github;
+        secondTitle = 'Engineer';
+    } else if (newEmployee[1].school) {
+        secondAdditional = newEmployee[1].school
+        secondTitle = 'Intern';
+    } else {
+        firstAdditional = newEmployee[1].officeNumber;
+        secondTitle = 'Manager';
+    }
+        
+        let secondHTML = ` <ul>
+    
+        <li>${secondName}</li>
+        <li>${secondTitle}</li>
+        <li>${secondID}</li>
+        <li>${secondEmail}</li>
+        <li>${secondAdditional}</li>
+        
+        </ul> `
+    
+    
+        return `
+        ${startHTML}
+    
+        <section class="container">
+    
+        <div class="card">
+    
+        <h1>Employee One</h1>
+    
+        ${firstHTML}
+       
+    
+        </div>
+
+        <div class="card">
+    
+        <h1>Employee Two</h1>
+
+        ${secondHTML}
+        
+        </div>
+    
+        </section>
+    
+        </body>
+        </html>
+        `
+    } else if (newEmployee.length == 1 ){
+    let firstName = newEmployee[0].emName;
+    let firstID = newEmployee[0].id;
+    let firstEmail = newEmployee[0].email;
+
+    let firstAdditional 
+    let firstTitle 
+
+    if (newEmployee[0].github) {
+        firstAdditional = newEmployee[0].github;
+        firstTitle = 'Engineer';
+    } else if (newEmployee[0].school) {
+        firstAdditional = newEmployee[0].school
+        firstTitle = 'Intern';
+    } else {
+        firstAdditional = newEmployee[0].officeNumber;
+        firstTitle = 'Manager';
+    }
+
+   
+    let firstHTML = ` 
+    <ul>
+    <li>${firstName}</li>
+    <li>${firstTitle}</li>
+    <li>${firstID}</li>
+    <li>${firstEmail}</li>
+    <li>${firstAdditional}</li> 
+    </ul> `
+
+    return `
+    ${startHTML}
+
+    <section class="container">
+
+    <div class="card">
+
+    <h1>Employee One</h1>
+
+   ${firstHTML}
+    
+    </div>
+
+    </section>
+
+    </body>
+    </html>
+    `
+} 
 }
 
 module.exports = generateHTML
